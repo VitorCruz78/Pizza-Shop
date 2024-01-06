98fb04f0aae7f4a7cc93455b244fb6ab0fa31f05
VitorCruz78 <vitorluiz15012006@gmail.com>
2024-01-05 23:25:33 -0300

    Componente de detalhes do pedido

--- pizzashop-web/src/pages/app/orders/order-table-row.tsx
+++ pizzashop-web/src/pages/app/orders/order-table-row.tsx
@@ -1,27 +1,34 @@
 import { ArrowRight, Search, X } from 'lucide-react'
 
 import { Button } from '../../../components/ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+} from '../../../components/ui/dialog'
 import { TableCell, TableRow } from '../../../components/ui/table'
+import { OrderDetails } from './orders-details'
 
 export function OrderTableRow() {
   return (
     <TableRow>
       <TableCell>
-        <Button variant="outline" size="xs">
-          <Search className="h-3 w-3" />
-          <span className="sr-only">Detalhes do pedido</span>
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="xs">
+              <Search className="h-3 w-3" />
+              <span className="sr-only">Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
+
+          <OrderDetails />
+        </Dialog>
       </TableCell>
       <TableCell className="font-mono text-xs font-medium">
         97ag09a7g8s97g
       </TableCell>
       <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
-      <TableCell>
-        <div className="flex items-center gap-2">
-          <span className="h-2 w-2 rounded-full bg-slate-400"></span>
-          Pendente
-        </div>
-      </TableCell>
+      <TableCell></TableCell>
       <TableCell className="font-medium">Vitor Luiz da Cruz</TableCell>
       <TableCell className="font-medium">R$ 78,00</TableCell>
       <TableCell>
